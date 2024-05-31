7b04f2cafea9d6c0bcbee905a331a16de5e79daa
sasapage25 <marciayanprincessa@gmail.com>
2024-06-01 02:07:50 +0700

    category page

--- EcommerceWeb/app/Http/Controllers/AdminController.php
+++ EcommerceWeb/app/Http/Controllers/AdminController.php
@@ -9,7 +9,9 @@ class AdminController extends Controller
 {
     public function category()
     {
-        return view('admin.category');
+        $data=category::all();
+
+        return view('admin.category',compact('data'));
     }
 
     public function add_category(Request $request)
@@ -21,5 +23,14 @@ class AdminController extends Controller
        $data->save();
 
        return redirect()->back()->with('message','Items Added Successfully');
+    }
+
+    public function delete_category($id)
+    {
+        $data=category::find($id);
+
+        $data->delete();
+
+        return redirect()->back()->with('message','Category Deleted');
     }
 }
