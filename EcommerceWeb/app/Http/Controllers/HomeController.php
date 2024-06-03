4ac901c23ea36bd54bdc4626da93b88c8cd8df78
GavrielJoseph <gavriel.535220049@stu.untar.ac.id>
2024-06-03 22:12:07 +0700

    product part progress

--- EcommerceWeb/app/Http/Controllers/HomeController.php
+++ EcommerceWeb/app/Http/Controllers/HomeController.php
@@ -29,4 +29,12 @@ class HomeController extends Controller
         return view('home.userpage',compact('product'));
         }
     }
+
+    public function product_details($id)
+    {
+        $product=product::find($id);
+
+        return view('home.product_details',compact('product'));
+    }
+
 }
