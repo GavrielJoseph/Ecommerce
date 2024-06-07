26d15ddc8cff2f3fcc7cb7da8503c155395f724c
GavrielJoseph <gavriel.535220049@stu.untar.ac.id>
2024-06-08 03:55:55 +0700

    verify email

--- EcommerceWeb/routes/web.php
+++ EcommerceWeb/routes/web.php
@@ -21,7 +21,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function(){
     return view('dashboard');
 })->name('dashboard');
 
-route::get('/redirect',[HomeController::class,'redirect']);
+route::get('/redirect',[HomeController::class,'redirect'])->middleware('auth','verified');
 
 route::get('/category',[AdminController::class,'category']);
 
