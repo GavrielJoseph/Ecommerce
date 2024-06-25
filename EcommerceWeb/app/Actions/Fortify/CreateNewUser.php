dc68dc5da434ac07ef328a9198b7587ec92dab6a
GavrielJoseph <gavriel.535220049@stu.untar.ac.id>
2024-06-25 15:17:13 +0700

    fix user id

--- EcommerceWeb/app/Actions/Fortify/CreateNewUser.php
+++ EcommerceWeb/app/Actions/Fortify/CreateNewUser.php
@@ -26,12 +26,21 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'address' => $input['address'],
-            'phone' => $input['phone'],
-            'password' => Hash::make($input['password']),
-        ]);
+        // Generate a unique user_id
+        do {
+            $user_id = random_int(1, PHP_INT_MAX);
+        } while (User::find($user_id) !== null);
+
+        $user = new User;
+        $user->id = $user_id;
+        $user->name = $input['name'];
+        $user->email = $input['email'];
+        $user->address = $input['address'] ?? null;
+        $user->phone = $input['phone'] ?? null;
+        $user->password = Hash::make($input['password']);
+
+        $user->save();
+
+        return $user;
     }
 }
