3c0b2b0339057ecf5ce6c4db54def80f377ed8f4
GavrielJoseph <gavriel.535220049@stu.untar.ac.id>
2024-06-27 23:31:10 +0700

    update logo di main page

--- EcommerceWeb/resources/views/home/footer.blade.php
+++ EcommerceWeb/resources/views/home/footer.blade.php
@@ -1,17 +1,19 @@
 <footer>
-         <div class="container">
-            <div class="row">
-               <div class="col-md-4">
-                   <div class="full">
-                      <div class="logo_footer">
-                        <a href="#"><img width="210" src="images/logo.png" alt="#" /></a>
-                      </div>
-                      <div class="information_f">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="full">
+                    <div class="logo_footer">
+                        <a href="#"><img width="150" src="{{ asset('admin/assets/images/GM.png') }}" alt="GM Logo" /></a>
+                    </div>
+                    <div class="information_f">
                         <!-- isi footer -->
-                        <p><strong>ADDRESS:</strong> Jembatan Niaga 2, Lt. 2 Blok KJ No.45, Jl. Mangga Dua Raya, Jakarta</p>
+                        <p><strong>ADDRESS:</strong> Jembatan Niaga 2, Lt. 2 Blok KJ No. 45, Jl. Mangga Dua Raya, Jakarta</p>
                         <p><strong>PHONE NUMBER:</strong> 0851 0002 3323</p>
-                        <p><strong>EMAIL:</strong> yourmain@gmail.com</p>
-                      </div>
-                   </div>
-               </div>
-      </footer>
+                        <p><strong>OWNER:</strong> YUSUF & TATI</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- EcommerceWeb/resources/views/home/header.blade.php
+++ EcommerceWeb/resources/views/home/header.blade.php
@@ -11,7 +11,9 @@
 <header class="header_section">
     <div class="container">
         <nav class="navbar navbar-expand-lg custom_nav-container">
-            <a class="navbar-brand" href="{{ url('/') }}">GM</a>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('admin/assets/images/GM.png') }}" alt="GM Logo" style="height: 80px;">
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class=""> </span>
             </button>
